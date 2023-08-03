014794abbd7f02bcda9dd785823b6e87f5477ba3
Geroldor <geraldo.antonio@uel.br>
2023-08-03 11:06:26 -0300

    jogador caro

--- rpg.py
+++ rpg.py
@@ -9,6 +9,7 @@ bottom_panel = 150
 screen_width = 800
 screen_height = 400 + bottom_panel
 screen = pygame.display.set_mode((screen_width, screen_height))
+
 pygame.display.set_caption('RPG')
 fpd = pygame.time.Clock()
 background = pygame.image.load("python/sprites/background.png").convert_alpha()
@@ -20,6 +21,7 @@ WHITE = (255, 255, 255)
 gray = (100, 100, 100)
 red = (255, 0, 0)
 green = (0, 155, 0)
+medium_green = (0, 100, 0)
 blue = (0, 0, 255)
 yellow = (255, 255, 0)
 
@@ -30,14 +32,18 @@ class Hero:
         self.strength = strength
         self.defense = defense
         self.alive = True
-
         self.image_list = [pygame.image.load("python/sprites/Warrior/Individual/idle/Warrior_Idle_1.png"), pygame.image.load("python/sprites/Warrior/Individual/idle/Warrior_Idle_2.png"), pygame.image.load("python/sprites/Warrior/Individual/idle/Warrior_Idle_3.png"), pygame.image.load("python/sprites/Warrior/Individual/idle/Warrior_Idle_4.png"), pygame.image.load("python/sprites/Warrior/Individual/idle/Warrior_Idle_5.png"), pygame.image.load("python/sprites/Warrior/Individual/idle/Warrior_Idle_6.png")]
+        for i in range(len(self.image_list)):
+            self.image_list[i] = pygame.transform.scale(self.image_list[i], ((self.image_list[i].get_width() * 3), (self.image_list[i].get_height() * 3)))
         self.image = self.image_list[0]
         self.image_index = 0
-        self.image = pygame.transform.scale(self.image, ((self.image.get_width() * 3), (self.image.get_height() * 3)))
+        self.update_index = pygame.time.get_ticks()
+        #self.image = pygame.transform.scale(self.image, ((self.image.get_width() * 3), (self.image.get_height() * 3)))
         self.rect = self.image.get_rect()
         self.rect.x = 90
         self.rect.y = 210
+        font = pygame.font.SysFont("Arial", 20)
+        self.text = font.render(self.name, True, WHITE)
 
 
     def attack(self, enemy):
@@ -46,39 +52,63 @@ class Hero:
         enemy.health -= self.strength - enemy.defense
 
     def update(self):
+        animation_cooldown = 100
+        self.image = self.image_list[self.image_index]
+        pygame.time.wait(120)
+        if pygame.time.get_ticks() - self.update_index > animation_cooldown:
+            self.image_index += 1
+            self.update_index = pygame.time.get_ticks()
+        if self.image_index >= len(self.image_list):
+            self.image_index = 0
         screen.blit(self.image, self.rect)
         pygame.draw.rect(screen, red, (500, 435, 280 - ((280 / 100) * (100 - self.health)), 20))
         pygame.draw.rect(screen, green, (500, 435, 280, 20))
+        pygame.draw.rect(screen, medium_green, (500, 435, 280, 15))
+        screen.blit(self.text, (423, 435))
         
 
-class Enemy:
+class knight:
     def __init__(self, name, health, strength, defense):
         self.name = name
         self.health = health
         self.strength = strength
         self.defense = defense
         self.alive = True
-
+        self.update_index = pygame.time.get_ticks()
         self.image_list = [pygame.image.load("python/sprites/Boss/idle/tile000.png"), pygame.image.load("python/sprites/Boss/idle/tile001.png"), pygame.image.load("python/sprites/Boss/idle/tile002.png"), pygame.image.load("python/sprites/Boss/idle/tile003.png")]
+        for i in range(len(self.image_list)):
+            self.image_list[i] = pygame.transform.scale(self.image_list[i], ((self.image_list[i].get_width() * 3), (self.image_list[i].get_height() * 3)))
         self.image = self.image_list[0]
         self.image_index = 0
         self.image = pygame.transform.scale(self.image, ((self.image.get_width() * 3), (self.image.get_height() * 3)))
         self.rect = self.image.get_rect()
         self.rect.x = 500
         self.rect.y = 155
+        font = pygame.font.SysFont("Arial", 20)
+        self.text = font.render(self.name, True, WHITE)
 
     def attack(self, hero):
         hero.health -= self.strength - hero.defense
 
     def update(self):
+        animation_cooldown = 100
+        self.image = self.image_list[self.image_index]
+        if pygame.time.get_ticks() - self.update_index > animation_cooldown:
+            self.image_index += 1
+            self.update_index = pygame.time.get_ticks()
+        if self.image_index >= len(self.image_list):
+            self.image_index = 0
         screen.blit(self.image, self.rect)
         pygame.draw.rect(screen, red, (500, 495, 280 - ((280 / 100) * (100 - self.health)), 20))
         pygame.draw.rect(screen, green, (500, 495, 280, 20))
+        pygame.draw.rect(screen, medium_green, (500, 495, 280, 15))
+        screen.blit(self.text, (425, 495))
+
 
 
 # Create entitys
 player = Hero("Player", 100, 10, 5)
-villain = Enemy("Villain", 100, 10, 5)
+villain = knight("Villain", 100, 10, 5)
 
 
 # Start the game loop
