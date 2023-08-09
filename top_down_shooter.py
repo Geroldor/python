6a55b45525781fa7f6cd3c642d7718b9a37b72b0
Geroldor <geraldo.antonio@uel.br>
2023-08-09 12:50:16 -0300

    kkkkkkkk

--- top_down_shooter.py
+++ top_down_shooter.py
@@ -10,23 +10,23 @@ clock = pygame.time.Clock()
 
 class Player:
     def __init__(self):
-        self.x = 480/2
-        self.y = 640/2
-        self.width = 5
-        self.height = 10
+        self.x = 480//2
+        self.y = 640//2
+        self.width = 32
+        self.height = 32
         
     def main(self, display):
-        pygame.draw.rect(display, (255, 255, 255), (self.x, self.y, self.width, self.height))
-
+        pygame.draw.circle(display, (255, 255, 255), (self.x, self.y), self.width)
 
 player = Player()
 
 while True:
     tela.fill((0, 0, 0))
-    
+    pygame.draw.circle(tela, (255, 255, 255), (player.x, player.y), player.width)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
             
     player.main(tela)       
+    clock.tick(30)
