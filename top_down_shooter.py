5795b372c1067526e3ad7fa27529e278a60f4536
Geroldor <geraldo.antonio@uel.br>
2023-08-23 16:52:08 -0300

    eu me odeio

--- top_down_shooter.py
+++ top_down_shooter.py
@@ -1,12 +1,13 @@
 import pygame
 from pygame.locals import *
 import random
+import math
 
 # Initialize pygame
 pygame.init()
 
 # Set up the window
-background_color = (0, 0, 0)
+background_color = (250, 250, 250)
 width = 800
 height = 600
 screen = pygame.display.set_mode((800, 600))
@@ -15,21 +16,141 @@ screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
 # Set up the player
-class Player:
+class Player(pygame.sprite.Sprite):
     def __init__(self, speed, damage):
+        
         self.x = width // 2
         self.y = height // 2
+        self.pos = pygame.math.Vector2(self.x, self.y)
         self.speed = speed
         self.damage = damage
-        self.image = pygame.image.load("python/tps/sprites/seta.png")
+        self.shoot_cooldown = 0
+        self.shooting = False
+        self.image = pygame.image.load("python/tps/sprites/seta-direita.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, ((self.image.get_width() // 2), (self.image.get_height() // 2)))
+        self.rect = self.image.get_rect()
+        self.base_image = self.image
+        self.base_rect = self.base_image.get_rect(center = self.pos)
+        
+    def player_rotation(self):
+        self.mouse_coords = pygame.mouse.get_pos()
+        self.x_change_mouse = self.mouse_coords[0] - self.rect.centerx
+        self.y_change_mouse = self.mouse_coords[1] - self.rect.centery
+        self.angle = math.degrees(math.atan2(self.y_change_mouse, self.x_change_mouse))
+        self.image = pygame.transform.rotate(self.base_image, -self.angle)
+        self.rect = self.image.get_rect(center = self.base_rect.center)
+        
+    def shoot(self):
+        if self.shoot_cooldown == 0:
+            self.shoot_cooldown = 20
+            self.bullet = Bullet(self.x, self.y, self.angle)
+            bullet_group.add(self.bullet)
+            
+
+    def user_input(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_w]:
+            self.y -= self.speed
+        if key[pygame.K_s]:
+            self.y += self.speed
+        if key[pygame.K_a]:
+            self.x -= self.speed
+        if key[pygame.K_d]:
+            self.x += self.speed
+            
+        if pygame.mouse.get_pressed() == (1, 0, 0) or key[pygame.K_SPACE]:
+            self.shooting = True
+            self.shoot()
+        else:
+            self.shooting = False
+    
+    
+    def move(self):
+        self.pos = pygame.math.Vector2(self.x, self.y)
+        self.base_rect.center = self.pos
+        self.rect.center = self.base_rect.center
     
     def update(self):
+        self.user_input()
+        self.move()
+        self.player_rotation()
+        screen.blit(self.image, self.rect)
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
+        
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, angle):
+        super().__init__()
+        
+        self.image = pygame.image.load("python/tps/sprites/ponto-final.png")
+        self.image = pygame.transform.scale(self.image, ((self.image.get_width() * 2), (self.image.get_height() * 2)))
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+        self.x_vel = math.cos(self.angle * (2*math.pi/360)) * 10
+        self.y_vel = math.sin(self.angle * (2*math.pi/360)) * 10
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+        self.rect.x = int(self.x)
+        self.rect.y = int(self.y)
+        
+    def update(self):
+        self.move()
+        screen.blit(self.image, self.rect)
+        if self.x < 0 or self.x > width:
+            self.kill()
+        
+        if self.rect.collidelist(all_sprites) > -1:
+            print("hit")
+            meteorite = all_sprites[self.rect.collidelist(all_sprites)]
+            meteorite.kill()
+            all_sprites.remove(meteorite)
+            bullet_group.remove(self)
+            self.kill
+            for i in range(random.randint(1, 3)):
+                metorite = Meteor(random.randint(0, width), random.randint(0, height), random.randint(-3, 2))
+                all_sprites.append(metorite)
+            
+        
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, x, y, speed):
+        super().__init__()
+        self.image = pygame.image.load("python/tps/sprites/meteoro1.png")
+        self.scale = random.randint(1, 3)
+        self.image = pygame.transform.scale(self.image, ((self.image.get_width() * self.scale), (self.image.get_height() * self.scale)))
+        self.rect = self.image.get_rect()
+        self.image_base = self.image
+        self.rect_base = self.image_base.get_rect()
+        self.x = x
+        self.y = y
+        self.speedx = speed
+        self.speedy = speed
+        
+    def move(self):
+        self.x += self.speedx
+        self.y += self.speedy
+        
+        if self.x < 0 or self.x > width:
+            self.speedx *= -1
+        if self.y < 0 or self.y > height:
+            self.speedy *= -1
+            
+    def update(self):
+        self.move()
         screen.blit(self.image, (self.x, self.y))
-
+        
 # Initialize game objects
-player = Player(5, 1)
-
+player = Player(10, 1)
+all_sprites = []
+bullet_group = pygame.sprite.Group()
+for i in range(random.randint(1, 10)):
+    metorite = Meteor(random.randint(0, width), random.randint(0, height), random.randint(-3, 2))
+    all_sprites.append(metorite)
+    
 # Set up the game loop
 running = True
 while running:
@@ -38,16 +159,6 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-
-    key = pygame.key.get_pressed()
-    if key[K_w]:
-        player.y -= player.speed
-    if key[K_s]:
-        player.y += player.speed
-    if key[K_a]:
-        player.x -= player.speed
-    if key[K_d]:
-        player.x += player.speed
     
     if player.x < 0:
         player.x = width
@@ -59,7 +170,11 @@ while running:
         player.y = 0
 
     # Update game state
+    screen.fill(background_color)
+    bullet_group.update()
+    for sprite in all_sprites:
+        sprite.update()
     player.update()
     
-    clock.tick(30)
     pygame.display.update()
+    clock.tick(30)
