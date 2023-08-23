90e2b12f37727a622d052063037b5824391f89a2
Geroldor <geraldo.antonio@uel.br>
2023-08-23 14:28:34 -0300

    a

--- top_down_shooter.py
+++ top_down_shooter.py
@@ -1,35 +1,44 @@
 import pygame
 from pygame.locals import *
-import sys
 import random
 
+# Initialize pygame
 pygame.init()
 
-tela = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
-backgiund = pygame.image.load("python\sprites\\background.png")
+# Set up the window
+background_color = (0, 0, 0)
+screen = pygame.display.set_mode((800, 600))
 
+# Set up the clock
+clock = pygame.time.Clock()
+
+# Set up the player
 class Player:
-    def __init__(self):
-        self.x = 640//2
-        self.y = 480//2
-        self.width = 32
-        self.height = 32
-        self.img = pygame.image.load("python\sprites\Warrior\Individual\idle\Warrior_Idle_1.png")        
-        self.img = pygame.transform.scale(self.img, (self.width * 3, self.height * 3))
+    def __init__(self, x, y, speed, damage):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.damage = damage
     
     def update(self):
-        tela.blit(self.img, (self.x, self.y))
-        
-player = Player()
+        pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, 20, 20))
 
-while True:
+# Initialize game objects
+player = Player(400, 300, 5, 1)
+
+# Set up the game loop
+running = True
+while running:
     
-    
+    # Handle events
     for event in pygame.event.get():
         if event.type == QUIT:
-            pygame.quit()
-            exit()
-            
+            running = False
+
+    # Update game state
     player.update()
+    
+    # Draw screen
+    screen.fill(background_color)
     clock.tick(30)
+    pygame.display.update()
